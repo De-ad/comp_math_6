ffd2d5243d93ea9575baa8244e22c979f35b4af2
Dead <space.gee.z@gmail.com>
2023-06-08 19:32:34 +0300

    add function radio

--- comp_math_6_front/src/components/UserDataInput.jsx
+++ comp_math_6_front/src/components/UserDataInput.jsx
@@ -6,18 +6,36 @@ import {setError} from "../store/slices/errorSlice";
 
 const UserDataInput = () => {
     const dispatch = useDispatch();
+    const [functionNum, setFunctionNum] = useState("1");
 
+    const handleRadioChange = (e) => {
+        setFunctionNum(e.target.value);
+    }
 
 
     return (
         <div>
-            <div>
-                <div>
-
-
-
+            <div className="grid">
+               <div className='flex'>
+                    <input type="radio" id="first"  name="function"
+                           checked={ functionNum === '1'}
+                    value="1" onChange={handleRadioChange}/>
+                    <label for="first"> func1 </label>
+               </div>
+                <div className='flex'>
+                    <input type="radio" id="second" name="function"
+                           checked={ functionNum === '2'}
+                    value="2" onChange={handleRadioChange}/>
+                    <label for="second"> func 2</label>
                 </div>
+                <div className='flex'>
+                    <input type="radio" id="third" name="function"
+                           checked={ functionNum === '3'}
+                    value="3" onChange={handleRadioChange}/>
+                    <label for="third"> func 3</label>
                 </div>
+
+            </div>
         </div>
     );
 };

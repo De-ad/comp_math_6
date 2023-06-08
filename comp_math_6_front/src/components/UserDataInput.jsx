35e3f0c9ab95ff2c7b50d119fa243f26912333ea
Dead <space.gee.z@gmail.com>
2023-06-08 19:59:18 +0300

    add equаtions

--- comp_math_6_front/src/components/UserDataInput.jsx
+++ comp_math_6_front/src/components/UserDataInput.jsx
@@ -20,21 +20,35 @@ const UserDataInput = () => {
                     <input type="radio" id="first"  name="function"
                            checked={ functionNum === '1'}
                     value="1" onChange={handleRadioChange}/>
-                    <label for="first"> func1 </label>
+                    <label for="first"> y' = y + (1 + x)y^2 </label>
                </div>
                 <div className='flex'>
                     <input type="radio" id="second" name="function"
                            checked={ functionNum === '2'}
                     value="2" onChange={handleRadioChange}/>
-                    <label for="second"> func 2</label>
+                    <label for="second"> (x + y)y' + y = 0</label>
                 </div>
                 <div className='flex'>
                     <input type="radio" id="third" name="function"
                            checked={ functionNum === '3'}
                     value="3" onChange={handleRadioChange}/>
-                    <label for="third"> func 3</label>
+                    <label for="third"> y^2 + x^2y' = xyy'</label>
                 </div>
-
+                <div>
+                    y(x0): <input type='text'/>
+                </div>
+                <div>
+                    x_0: <input type='text'/>
+                </div>
+                <div>
+                    x_n: <input type='text'/>
+                </div>
+                <div>
+                    h: <input type='text'/>
+                </div>
+                <div>
+                    eps: <input type='text'/>
+                </div>
             </div>
         </div>
     );
